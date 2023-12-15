4c7b1b148cae628e0c6ad190e45c512d9a39d617
johntrehearn <johntrehearnbc@gmail.com>
2023-12-15 21:11:04 +0200

    slight updates to input box

--- src/wordEntry.jsx
+++ src/wordEntry.jsx
@@ -3,7 +3,7 @@
 function Word({checkPal, wordEnt}) {
     return (
         <>
-        <input id="word" type='text' placeholder='The magic starts here' value={wordEnt}
+        <input id="word" type='text' placeholder='Please enter your word, number or phrase' value={wordEnt}
         onChange={checkPal}/>
         {/* <button onClick={checkPal}>Check Palindrome</button> */}
         </>
